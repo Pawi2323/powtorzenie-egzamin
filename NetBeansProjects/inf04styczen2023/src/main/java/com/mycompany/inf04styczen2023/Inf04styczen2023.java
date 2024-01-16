781740a6d01158c37c440d8cfdedf469a1e9a14e
Pawel <49004622+Pawi2323@users.noreply.github.com>
2024-01-16 17:26:52 +0100

    Add files via upload

---
+++ NetBeansProjects/inf04styczen2023/src/main/java/com/mycompany/inf04styczen2023/Inf04styczen2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inf04styczen2023;
+
+/**
+ *
+ * @author Pawel
+ */
+public class Inf04styczen2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
